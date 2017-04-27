ba4c88dace508782158872b63ac39231feac68e5
zx88cvb <295389754@qq.com>
2017-04-27 16:49:46 +0800

    Add files via upload

---
+++ weixinTest/src/org/wx/servlet/CoreServlet.java
@@ -0,0 +1,49 @@
+package org.wx.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CoreServlet
+ */
+@WebServlet("/CoreServlet")
+public class CoreServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 微信加密签名  
+        String signature = request.getParameter("signature");  
+        // 时间戳  
+        String timestamp = request.getParameter("timestamp");  
+        // 随机数  
+        String nonce = request.getParameter("nonce");  
+        // 随机字符串  
+        String echostr = request.getParameter("echostr");  
+  
+        PrintWriter out = response.getWriter();  
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败  
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {  
+            out.print(echostr);  
+        }  
+        out.close();  
+        out = null;  
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
